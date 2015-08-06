4f4a52fec64560ebf4a0402d87169655ce5f93bf
1000ch <shogo.sensui@gmail.com>
2015-08-06 14:21:13 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,64 @@
+"use babel";
+
+import fs      from 'fs';
+import path    from 'path';
+import postcss from 'postcss';
+import cssfmt  from 'cssfmt';
+
+export let config = {
+  executeOnSave: {
+    title: 'Execute on save',
+    description: 'Formatting CSS on save.',
+    type: 'boolean',
+    default: false
+  }
+};
+
+const executeOnSave = () => atom.config.get('cssfmt.executeOnSave');
+
+const execute = () => {
+
+  const editor = atom.workspace.getActiveTextEditor();
+
+  if (!editor) {
+    return;
+  }
+
+  let text = editor.getText();
+  let selectedText = editor.getSelectedText();
+  let formatter = postcss().use(cssfmt());
+
+  if (selectedText.length !== 0) {
+    try {
+      editor.setTextInBufferRange(
+        editor.getSelectedBufferRange(),
+        formatter.process(selectedText).css
+      );
+    } catch (e) {}
+  } else {
+    try {
+      editor.setText(formatter.process(text).css);
+    } catch (e) {}
+  }
+};
+
+let editorObserver = null;
+
+export const activate = (state) => {
+
+  atom.commands.add('atom-workspace', 'cssfmt:execute', () => {
+    execute();
+  });
+
+  editorObserver = atom.workspace.observeTextEditors((editor) => {
+    editor.getBuffer().onWillSave(() => {
+      if (executeOnSave()) {
+        execute();
+      }
+    });
+  });
+};
+
+export const deactivate = () => {
+  editorObserver.dispose();
+};
---
+++ readme.md
@@ -0,0 +1,21 @@
+# [atom-cssfmt](https://atom.io/packages/cssfmt)
+
+Format your JavaScript using [cssfmt](https://github.com/morishitter/cssfmt).
+
+## Installation
+
+```bash
+$ apm install cssfmt
+```
+
+## Usage
+
+- Press ctrl + alt + c
+- Packages > cssfmt > Format CSS
+- Right Click > Format CSS
+
+Selected text will be formatted if there is selection.
+
+## License
+
+MIT: http://1000ch.mit-license.org
